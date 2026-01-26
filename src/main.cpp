f086d7505a84d9778599d56f9213f088a3473a5a
mengying <nibabatang@gmail.com>
2026-01-27 00:41:36 +0800

    feat 添加帧数显示

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
-#include <iostream>
-#include <string>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
+#include <chrono>
+#include <iostream>
+#include <string>
 
-int main(int argc, char *argv[])
+using MyClock = std::chrono::steady_clock;
+
+void UpdateFpsText(SDL_Renderer* renderer, TTF_Font* font, SDL_Texture*& texture, SDL_Rect& rect,
+                   double fps)
+{
+    if (texture)
+    {
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
+    }
+
+    SDL_Color black{0, 0, 0, 255};
+    std::string text = "FPS: " + std::to_string(static_cast<int>(fps));
+
+    SDL_Surface* surface = TTF_RenderUTF8_Blended(font, text.c_str(), black);
+
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    rect = {10, 10, surface->w, surface->h};
+
+    SDL_FreeSurface(surface);
+}
+
+int main(int argc, char* argv[])
 {
     std::cout << "hello_world!" << std::endl;
 
@@ -17,9 +41,10 @@ int main(int argc, char *argv[])
     }
 
     // 创建窗口
-    SDL_Window *window = SDL_CreateWindow("hello world", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("hello world", SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
     // 创建渲染器
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     // sdl_image初始化
     if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) != (IMG_INIT_JPG | IMG_INIT_PNG))
@@ -28,7 +53,7 @@ int main(int argc, char *argv[])
         return 1;
     }
     // 加载图片
-    SDL_Texture *texture = IMG_LoadTexture(renderer, "../assets/image/bonus_life.png");
+    SDL_Texture* texture = IMG_LoadTexture(renderer, "../assets/image/bonus_life.png");
 
     // sdl_mix初始化
     if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG) != (MIX_INIT_MP3 | MIX_INIT_OGG))
@@ -40,7 +65,7 @@ int main(int argc, char *argv[])
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
     // 载入音频
-    Mix_Music *music = Mix_LoadMUS("../assets/music/03_Racing_Through_Asteroids_Loop.ogg");
+    Mix_Music* music = Mix_LoadMUS("../assets/music/03_Racing_Through_Asteroids_Loop.ogg");
     // 播放音频
     Mix_PlayMusic(music, -1);
 
@@ -51,12 +76,19 @@ int main(int argc, char *argv[])
         return 1;
     }
     // 载入字体
-    TTF_Font *font = TTF_OpenFont("../assets/font/VonwaonBitmap-16px.ttf", 24);
+    TTF_Font* font = TTF_OpenFont("../assets/font/VonwaonBitmap-16px.ttf", 24);
     SDL_Color color = {0, 0, 0, 255};
-    SDL_Surface *surface = TTF_RenderUTF8_Solid(font, "hello_world 你好世界", color);
+    SDL_Surface* surface = TTF_RenderUTF8_Solid(font, "hello_world 你好世界", color);
     // 创建纹理
-    SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, surface);
     // todo
+
+    // 计算每秒帧数
+    auto fps_start = MyClock::now();
+    size_t fps_count{0};
+    // 用于显示每秒帧数的texture,rect
+    SDL_Texture* fps_texture{NULL};
+    SDL_Rect fps_showRect{};
     // 绘制循环
     while (true)
     {
@@ -81,6 +113,23 @@ int main(int argc, char *argv[])
         // 绘制文字
         SDL_Rect text{300, 300, surface->w, surface->h};
         SDL_RenderCopy(renderer, text_texture, NULL, &text);
+
+        fps_count++;
+        auto now = MyClock::now();
+        std::chrono::duration<double> elapsed = now - fps_start;
+        if (elapsed.count() >= 1.0)
+        {
+            double fps = fps_count / elapsed.count();
+            std::cout << "fps: " << fps << std::endl;
+            UpdateFpsText(renderer, font, fps_texture, fps_showRect, fps);
+            fps_count = 0;
+            fps_start = now;
+        }
+        if (fps_texture)
+        {
+            SDL_RenderCopy(renderer, fps_texture, NULL, &fps_showRect);
+        }
+
         // 更新屏幕
         SDL_RenderPresent(renderer);
     }
