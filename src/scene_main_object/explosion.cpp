441f3b05ebb438d00bce24bea82bcfb1608715b4
mengying <nibabatang@gmail.com>
2026-07-19 23:33:00 +0800

    修复爆炸渲染尺寸，将帧大小放大两倍以匹配预期效果

--- src/scene_main_object/explosion.cpp
+++ src/scene_main_object/explosion.cpp
@@ -14,7 +14,7 @@ Explosion::Explosion(float x, float y, int width, int height, std::chrono::stead
 void Explosion::render(SDL_Renderer* renderer)
 {
     SDL_Rect src_rect = {current_frame * frame_width, 0, frame_width, frame_height};
-    SDL_Rect dst_rect = {(int)position.x, (int)position.y, frame_width, frame_height};
+    SDL_Rect dst_rect = {(int)position.x, (int)position.y, frame_width * 2, frame_height * 2};
     SDL_RenderCopy(renderer, texture_, &src_rect, &dst_rect);
 }
 
